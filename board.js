47345ed2d3988ada21558b593742277bf4725da6
HackwareBro <hackwarebro@gmail.com>
2021-08-21 09:14:09 +0500

    token spreading algorithm has been implemented and turn by turn also implemented

--- board.js
+++ board.js
@@ -1,3 +1,14 @@
+function _2dTo1d(row,col){    //function to convert rows and columns to a single number to deal with ids of places
+    return row*10+col;
+}
+function _1dTo2d(val){ 
+    if (typeof val == "string"){
+        val = parseInt(val.slice(1,3))  //if string comes with a prefix character e.g id = 'c12'
+    }
+    row = Math.floor(val/10);
+    col = val%10;
+    return {row, col};
+}
 function createGameArea(){
     for (let i = 0; i < 10; i++){
         row = document.createElement('tr');
@@ -5,12 +16,20 @@ function createGameArea(){
         document.getElementsByTagName('table')[0].appendChild(row);
         for (let j = 0; j < 10; j++){
             col = document.createElement('td')
-            col.id = 'c'+ (i*10 + j);
+            col.id = 'c'+ _2dTo1d(i,j);
             col.className = 'empty'
-            col.setAttribute('onclick','move(this)')
+            col.setAttribute('onclick','play(this)')
             // col.appendChild(document.createTextNode('ta1'))  just for testing
             document.querySelector('tr#r'+i).appendChild(col);
         }
     }
+    // setting start position for both players
+    opponent = document.getElementById('c' + _2dTo1d(1,1))
+    opponent.className='opponent';
+    opponent.innerHTML = '3'
+    player = document.getElementById('c' + _2dTo1d(8,8))
+    player.className='player';
+    player.innerHTML='3';
+
 }
 createGameArea();
--- script.js
+++ script.js
@@ -1,3 +1,93 @@
-function move(element){
-    element.className = "player";
+first_turn = false;
+first_moves = {up:false,down:false,left:false,right:false};
+
+function play(element){
+    if(first_turn != true && element.className == 'player'){
+        move(element,Object.assign({},first_moves),'player')
+        first_turn = true;
+    }
+    else if(first_turn == true && element.className == 'opponent'){
+        move(element,Object.assign({},first_moves),'opponent')
+        first_turn = false;
+    }
+}
+
+function move(element, moves, token){
+    if(element.className == token){        
+        let number = parseInt(element.innerHTML, 10)    
+        if(number > 2){            
+            element.className = 'empty';
+            element.innerHTML = ''
+            place = _1dTo2d(element.id);  
+
+            //now spread the tokens on four places
+            if(place.row > 0){
+                var up = document.getElementById('c'+_2dTo1d(place.row-1,place.col));
+                if(up.className == 'empty'){    
+                    up.className = token
+                    up.innerHTML = '1'
+                    moves.up = false
+                }
+                else{
+                    moves.up = true 
+                }
+                
+            }  
+            if(place.row < 9){
+                var down = document.getElementById('c'+_2dTo1d(place.row+1,place.col));
+                if(down.className == 'empty'){    
+                    down.className = token
+                    down.innerHTML = '1'
+                    moves.down = false
+                }
+                else{
+                    moves.down = true 
+                }
+            }
+            if(place.col > 0){
+                var left = document.getElementById('c'+_2dTo1d(place.row,place.col-1));
+                
+                if(left.className == 'empty'){    
+                    left.className = token
+                    left.innerHTML = '1'
+                    moves.left = false
+                }
+                else{
+                    moves.left = true 
+                }
+            }
+            if(place.col < 9){
+                var right = document.getElementById('c'+_2dTo1d(place.row,place.col+1));
+                if(right.className == 'empty'){    
+                    right.className = token
+                    right.innerHTML = '1'
+                    moves.right = false
+                }
+                else{
+                    moves.right = true 
+                }
+            }
+            if(moves.left == true){
+                console.log(moves)
+                move(left,true,Object.assign({},moves));
+            }
+            if(moves.right == true){
+                console.log(right + ' true')
+                move(right,true,Object.assign({},moves));
+            }
+            if(moves.up == true){
+                console.log(up + ' true')
+                move(up,true,Object.assign({},moves));
+            }
+            if(moves.down == true){
+                console.log(down + ' true')
+                move(down,true,Object.assign({},moves));
+            }
+        }
+        else{
+            val = element.innerHTML
+            element.innerHTML = parseInt(val) + 1
+        }
+    }
+    
 }
