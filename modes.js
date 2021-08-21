e24318e44c16b767f6c5dce2594480303e3b07de
HackwareBro <hackwarebro@gmail.com>
2021-08-21 11:22:14 +0500

    main screen has been implemented

---
+++ modes.js
@@ -0,0 +1,102 @@
+function setMode(){
+    mode = document.querySelector('input[name="mode"]:checked').value;
+    if (mode == '2p'){
+        screen = document.getElementsByClassName('screen')[0];
+        screen.className = 'screen hide'    
+    }
+    else{
+        document.getElementById('temp-msg').innerHTML = "CPU mode will available very soon!, it's under development."
+    }
+
+}
+
+
+//player Algorithm
+function move(element, moves, token){
+    if(element.className != 'empty'){        
+        let number = parseInt(element.innerHTML, 10)    
+        if(number > 2){            
+            element.className = 'empty';
+            element.innerHTML = ''
+            changeTokenNumber(token,-1);
+            place = _1dTo2d(element.id);  
+
+            //now spread the tokens on four places
+            if(place.row > 0){
+                var up = document.getElementById('c'+_2dTo1d(place.row-1,place.col));
+                if(up.className == 'empty'){    
+                    up.className = token
+                    up.innerHTML = '1'
+                    changeTokenNumber(token,1);
+                    moves.up = false
+                }
+                else{
+                    moves.up = true 
+                }
+                
+            }  
+            if(place.row < 9){
+                var down = document.getElementById('c'+_2dTo1d(place.row+1,place.col));
+                if(down.className == 'empty'){    
+                    down.className = token
+                    down.innerHTML = '1'
+                    changeTokenNumber(token,1);
+                    moves.down = false
+                }
+                else{
+                    moves.down = true 
+                }
+            }
+            if(place.col > 0){
+                var left = document.getElementById('c'+_2dTo1d(place.row,place.col-1));
+                
+                if(left.className == 'empty'){    
+                    left.className = token
+                    left.innerHTML = '1'
+                    changeTokenNumber(token,1);
+                    moves.left = false
+                }
+                else{
+                    moves.left = true 
+                }
+            }
+            if(place.col < 9){
+                var right = document.getElementById('c'+_2dTo1d(place.row,place.col+1));
+                if(right.className == 'empty'){    
+                    right.className = token
+                    right.innerHTML = '1'
+                    changeTokenNumber(token,1);
+                    moves.right = false
+                }
+                else{
+                    moves.right = true 
+                }
+            }
+            if(moves.left == true){
+                console.log(moves)
+                move(left,Object.assign({},moves),token);
+            }
+            if(moves.right == true){
+                console.log(right + ' true')
+                move(right,Object.assign({},moves),token);
+            }
+            if(moves.up == true){
+                console.log(up + ' true')
+                move(up,Object.assign({},moves),token);
+            }
+            if(moves.down == true){
+                console.log(down + ' true')
+                move(down,Object.assign({},moves),token);
+            }
+        }
+        else{
+            if(element.className != token){
+                element.className = token //to conquer other's places
+                changeTokenNumber(token,1)
+            }
+            val = element.innerHTML
+            element.innerHTML = parseInt(val) + 1
+        }
+    }
+    
+}
--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function changeTurn(){
         turn.innerHTML = 'Blue';
     }
 }
+
 function changeTokenNumber(token, value){
     if(token == 'player'){
         let blue_count = document.getElementById('blue-count');
@@ -24,10 +25,12 @@ function changeTokenNumber(token, value){
         empty_spaces.innerHTML = parseInt(empty_spaces.innerHTML) - value;
     }
 }
+
 function incMoves(){
     let move_count = document.getElementById('move-count');
     move_count.innerHTML = parseInt(move_count.innerHTML) + 1
 }
+
 function play(element){
     if(first_turn != true && element.className == 'player'){
         move(element,Object.assign({},first_moves),'player')
@@ -41,94 +44,4 @@ function play(element){
         first_turn = false;
         changeTurn();
     }
-}
-
-//Gameplay Algorithm
-function move(element, moves, token){
-    if(element.className != 'empty'){        
-        let number = parseInt(element.innerHTML, 10)    
-        if(number > 2){            
-            element.className = 'empty';
-            element.innerHTML = ''
-            changeTokenNumber(token,-1);
-            place = _1dTo2d(element.id);  
-
-            //now spread the tokens on four places
-            if(place.row > 0){
-                var up = document.getElementById('c'+_2dTo1d(place.row-1,place.col));
-                if(up.className == 'empty'){    
-                    up.className = token
-                    up.innerHTML = '1'
-                    changeTokenNumber(token,1);
-                    moves.up = false
-                }
-                else{
-                    moves.up = true 
-                }
-                
-            }  
-            if(place.row < 9){
-                var down = document.getElementById('c'+_2dTo1d(place.row+1,place.col));
-                if(down.className == 'empty'){    
-                    down.className = token
-                    down.innerHTML = '1'
-                    changeTokenNumber(token,1);
-                    moves.down = false
-                }
-                else{
-                    moves.down = true 
-                }
-            }
-            if(place.col > 0){
-                var left = document.getElementById('c'+_2dTo1d(place.row,place.col-1));
-                
-                if(left.className == 'empty'){    
-                    left.className = token
-                    left.innerHTML = '1'
-                    changeTokenNumber(token,1);
-                    moves.left = false
-                }
-                else{
-                    moves.left = true 
-                }
-            }
-            if(place.col < 9){
-                var right = document.getElementById('c'+_2dTo1d(place.row,place.col+1));
-                if(right.className == 'empty'){    
-                    right.className = token
-                    right.innerHTML = '1'
-                    changeTokenNumber(token,1);
-                    moves.right = false
-                }
-                else{
-                    moves.right = true 
-                }
-            }
-            if(moves.left == true){
-                console.log(moves)
-                move(left,Object.assign({},moves),token);
-            }
-            if(moves.right == true){
-                console.log(right + ' true')
-                move(right,Object.assign({},moves),token);
-            }
-            if(moves.up == true){
-                console.log(up + ' true')
-                move(up,Object.assign({},moves),token);
-            }
-            if(moves.down == true){
-                console.log(down + ' true')
-                move(down,Object.assign({},moves),token);
-            }
-        }
-        else{
-            if(element.className != token){
-                element.className = token //to conquer other's places
-                changeTokenNumber(token,1)
-            }
-            val = element.innerHTML
-            element.innerHTML = parseInt(val) + 1
-        }
-    }
-    
 }
